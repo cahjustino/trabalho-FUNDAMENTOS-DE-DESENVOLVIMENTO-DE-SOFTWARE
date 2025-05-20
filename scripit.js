ac6b5aa569bf3c302d2483dca806afe9293d106b
carlajustino <66473182+cahjustino@users.noreply.github.com>
2025-05-19 21:49:13 -0300

    Add files via upload
    codificação do trabalho 1

---
+++ scripit.js
@@ -0,0 +1,5 @@
+
+const button = document.getElementById("meuBotao");
+button.addEventListener("click", function() {
+    alert("Você é o protagonista da sua própria história,com a capacidade de moldar e dar forma ao seu próprio caminho.!");
+});
